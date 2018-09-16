89c41054811261ef89cb5d45deb10036a1df743d
Jakub Kus <jakkus@op.pl>
2018-09-16 14:03:19 +0200

    Initial commit

---
+++ src/com/kusmierczyk/Main.java
@@ -0,0 +1,39 @@
+package com.kusmierczyk;
+
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        File file = new File("text.txt");
+
+        BufferedReader br = null;
+        try {
+            FileReader fr = new FileReader(file);
+            br = new BufferedReader(fr);
+
+            String line;
+            while((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+
+
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + file.toString());
+        } catch (IOException e) {
+            System.out.println("Unable to read file: " + file.toString());
+        }
+        finally{
+            try {
+                br.close();
+            } catch (IOException e) {
+                System.out.println("Unable to close file : " + file.toString());
+            }catch (NullPointerException ex){
+
+            }
+        }
+
+
+    }
+}
